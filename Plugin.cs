6d20e3f52688099d3de05ddfd63fed03196456f8
AzumattDev <azumattgaming@gmail.com>
2022-06-04 02:06:50 -0400

    Init

---
+++ Plugin.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using UnityEngine;
+
+namespace ServerSyncModTemplate
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ServerSyncModTemplatePlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "ServerSyncModTemplate";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "{Azumatt}";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        internal static string ConnectionError = "";
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource ServerSyncModTemplateLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                ServerSyncModTemplateLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                ServerSyncModTemplateLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                ServerSyncModTemplateLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<bool>? _serverConfigLocked;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+        
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+    }
+}
