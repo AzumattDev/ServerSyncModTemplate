9c5422ab75b7a41bbf3bcca1f4f1138e8dd8b7b9
AzumattDev <azumattgaming@gmail.com>
2023-05-27 21:06:17 -0400

    Update the template a little for more dynamic pathing and other shit.

--- Plugin.cs
+++ Plugin.cs
@@ -37,6 +37,9 @@ namespace ServerSyncModTemplate
 
         public void Awake()
         {
+            // Uncomment the line below to use the LocalizationManager for localizing your mod.
+            //Localizer.Load(); // Use this to initialize the LocalizationManager (for more information on LocalizationManager, see the LocalizationManager documentation https://github.com/blaxxun-boop/LocalizationManager#example-project).
+
             _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
                 "If on, the configuration is locked and can be changed by server admins only.");
             _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
@@ -123,7 +126,7 @@ namespace ServerSyncModTemplate
             public override bool IsValid(object value) => true;
 
             public override string ToDescriptionString() =>
-                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+                "# Acceptable values: " + string.Join(", ", UnityInput.Current.SupportedKeyCodes);
         }
 
         #endregion
