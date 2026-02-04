9263d4597b6b68e8bb82d921469b93d8c0dc1671
AzumattDev <azumattgaming@gmail.com>
2026-02-04 11:54:28 -0500

    Update LocalizationManager.cs and project

--- Plugin.cs
+++ Plugin.cs
@@ -11,182 +11,185 @@ using JetBrains.Annotations;
 using ServerSync;
 using UnityEngine;
 
-namespace ServerSyncModTemplate
+namespace ServerSyncModTemplate;
+
+[BepInPlugin(ModGUID, ModName, ModVersion)]
+public class ServerSyncModTemplatePlugin : BaseUnityPlugin
 {
-    [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class ServerSyncModTemplatePlugin : BaseUnityPlugin
+    internal const string ModName = "ServerSyncModTemplate";
+    internal const string ModVersion = "1.0.0";
+    internal const string Author = "{Azumatt}";
+    private const string ModGUID = $"{Author}.{ModName}";
+    private static string ConfigFileName = $"{ModGUID}.cfg";
+    private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+    internal static string ConnectionError = "";
+    private readonly Harmony _harmony = new(ModGUID);
+    public static readonly ManualLogSource ServerSyncModTemplateLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+    private static readonly ConfigSync ConfigSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+    private FileSystemWatcher _watcher;
+    private readonly object _reloadLock = new();
+    private DateTime _lastConfigReloadTime;
+    private const long RELOAD_DELAY = 10000000; // One second
+
+    public enum Toggle
     {
-        internal const string ModName = "ServerSyncModTemplate";
-        internal const string ModVersion = "1.0.0";
-        internal const string Author = "{Azumatt}";
-        private const string ModGUID = $"{Author}.{ModName}";
-        private static string ConfigFileName = $"{ModGUID}.cfg";
-        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
-        internal static string ConnectionError = "";
-        private readonly Harmony _harmony = new(ModGUID);
-        public static readonly ManualLogSource ServerSyncModTemplateLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
-        private static readonly ConfigSync ConfigSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
-        private FileSystemWatcher _watcher;
-        private readonly object _reloadLock = new();
-        private DateTime _lastConfigReloadTime;
-        private const long RELOAD_DELAY = 10000000; // One second
-
-        public enum Toggle
-        {
-            On = 1,
-            Off = 0
-        }
-
-        public void Awake()
-        {
-            bool saveOnSet = Config.SaveOnConfigSet;
-            Config.SaveOnConfigSet = false;
-
-            // Uncomment the line below to use the LocalizationManager for localizing your mod.
-            // Make sure to populate the English.yml file in the translation folder with your keys to be localized and the values associated before uncommenting!.
-            //Localizer.Load(); // Use this to initialize the LocalizationManager (for more information on LocalizationManager, see the LocalizationManager documentation https://github.com/blaxxun-boop/LocalizationManager#example-project).
-
-            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On, "If on, the configuration is locked and can be changed by server admins only.");
-            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
-
-
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            _harmony.PatchAll(assembly);
-            SetupWatcher();
-
-            Config.Save();
-            if (saveOnSet)
-            {
-                Config.SaveOnConfigSet = saveOnSet;
-            }
-        }
-
-        private void OnDestroy()
-        {
-            SaveWithRespectToConfigSet();
-            _watcher?.Dispose();
-        }
-
-        private void SetupWatcher()
-        {
-            _watcher = new FileSystemWatcher(Paths.ConfigPath, ConfigFileName);
-            _watcher.Changed += ReadConfigValues;
-            _watcher.Created += ReadConfigValues;
-            _watcher.Renamed += ReadConfigValues;
-            _watcher.IncludeSubdirectories = true;
-            _watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
-            _watcher.EnableRaisingEvents = true;
-        }
-
-        private void ReadConfigValues(object sender, FileSystemEventArgs e)
-        {
-            DateTime now = DateTime.Now;
-            long time = now.Ticks - _lastConfigReloadTime.Ticks;
-            if (time < RELOAD_DELAY)
-            {
-                return;
-            }
-            lock (_reloadLock)
-            {
-                if (!File.Exists(ConfigFileFullPath))
-                {
-                    ServerSyncModTemplateLogger.LogWarning("Config file does not exist. Skipping reload.");
-                    return;
-                }
-
-                try
-                {
-                    ServerSyncModTemplateLogger.LogDebug("Reloading configuration...");
-                    SaveWithRespectToConfigSet(true);
-                    ServerSyncModTemplateLogger.LogInfo("Configuration reload complete.");
-                }
-                catch (Exception ex)
-                {
-                    ServerSyncModTemplateLogger.LogError($"Error reloading configuration: {ex.Message}");
-                }
-            }
-            _lastConfigReloadTime = now;
-        }
-
-        private void SaveWithRespectToConfigSet(bool reload = false)
-        {
-            bool originalSaveOnSet = Config.SaveOnConfigSet;
-            Config.SaveOnConfigSet = false;
-            if (reload)
-                Config.Reload();
-            Config.Save();
-            if (originalSaveOnSet)
-            {
-                Config.SaveOnConfigSet = originalSaveOnSet;
-            }
-        }
-
-
-        #region ConfigOptions
-
-        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
-
-        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
-        {
-            ConfigDescription extendedDescription = new(description.Description + (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"), description.AcceptableValues, description.Tags);
-            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
-            //var configEntry = Config.Bind(group, name, value, description);
-
-            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
-            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
-
-            return configEntry;
-        }
-
-        private ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true)
-        {
-            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
-        }
-
-        private class ConfigurationManagerAttributes
-        {
-            [UsedImplicitly] public int? Order = null!;
-            [UsedImplicitly] public bool? Browsable = null!;
-            [UsedImplicitly] public string? Category = null!;
-            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer = null!;
-        }
-
-        class AcceptableShortcuts : AcceptableValueBase
-        {
-            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
-            {
-            }
-
-            public override object Clamp(object value) => value;
-            public override bool IsValid(object value) => true;
-
-            public override string ToDescriptionString() => $"# Acceptable values: {string.Join(", ", UnityInput.Current.SupportedKeyCodes)}";
-        }
-
-        #endregion
+        On = 1,
+        Off = 0
     }
 
-    public static class KeyboardExtensions
+    public void Awake()
     {
-        public static bool IsKeyDown(this KeyboardShortcut shortcut)
+        bool saveOnSet = Config.SaveOnConfigSet;
+        Config.SaveOnConfigSet = false;
+
+        // Uncomment the line below to use the LocalizationManager for localizing your mod.
+        // Make sure to populate the English.yml file in the translation folder with your keys to be localized and the values associated before uncommenting!.
+        //Localizer.Load(); // Use this to initialize the LocalizationManager (for more information on LocalizationManager, see the LocalizationManager documentation https://github.com/blaxxun-boop/LocalizationManager#example-project).
+
+        _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On, "If on, the configuration is locked and can be changed by server admins only.");
+        _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        _harmony.PatchAll(assembly);
+        SetupWatcher();
+
+        Config.Save();
+        if (saveOnSet)
+        {
+            Config.SaveOnConfigSet = saveOnSet;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SaveWithRespectToConfigSet();
+        _watcher?.Dispose();
+    }
+
+    private void SetupWatcher()
+    {
+        _watcher = new FileSystemWatcher(Paths.ConfigPath, ConfigFileName);
+        _watcher.Changed += ReadConfigValues;
+        _watcher.Created += ReadConfigValues;
+        _watcher.Renamed += ReadConfigValues;
+        _watcher.IncludeSubdirectories = true;
+        _watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+        _watcher.EnableRaisingEvents = true;
+    }
+
+    private void ReadConfigValues(object sender, FileSystemEventArgs e)
+    {
+        DateTime now = DateTime.Now;
+        long time = now.Ticks - _lastConfigReloadTime.Ticks;
+        if (time < RELOAD_DELAY)
+        {
+            return;
+        }
+
+        lock (_reloadLock)
+        {
+            if (!File.Exists(ConfigFileFullPath))
+            {
+                ServerSyncModTemplateLogger.LogWarning("Config file does not exist. Skipping reload.");
+                return;
+            }
+
+            try
+            {
+                ServerSyncModTemplateLogger.LogDebug("Reloading configuration...");
+                SaveWithRespectToConfigSet(true);
+                ServerSyncModTemplateLogger.LogInfo("Configuration reload complete.");
+            }
+            catch (Exception ex)
+            {
+                ServerSyncModTemplateLogger.LogError($"Error reloading configuration: {ex.Message}");
+            }
+        }
+
+        _lastConfigReloadTime = now;
+    }
+
+    private void SaveWithRespectToConfigSet(bool reload = false)
+    {
+        bool originalSaveOnSet = Config.SaveOnConfigSet;
+        Config.SaveOnConfigSet = false;
+        if (reload)
+            Config.Reload();
+        Config.Save();
+        if (originalSaveOnSet)
+        {
+            Config.SaveOnConfigSet = originalSaveOnSet;
+        }
+    }
+
+
+    #region ConfigOptions
+
+    private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+
+    private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+    {
+        ConfigDescription extendedDescription = new(description.Description + (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"), description.AcceptableValues, description.Tags);
+        ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+        //var configEntry = Config.Bind(group, name, value, description);
+
+        SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+        syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+        return configEntry;
+    }
+
+    private ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true)
+    {
+        return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+    }
+
+    private class ConfigurationManagerAttributes
+    {
+        [UsedImplicitly] public int? Order = null!;
+        [UsedImplicitly] public bool? Browsable = null!;
+        [UsedImplicitly] public string? Category = null!;
+        [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer = null!;
+    }
+
+    class AcceptableShortcuts() : AcceptableValueBase(typeof(KeyboardShortcut))
+    {
+        public override object Clamp(object value) => value;
+        public override bool IsValid(object value) => true;
+
+        public override string ToDescriptionString() => $"# Acceptable values: {string.Join(", ", UnityInput.Current.SupportedKeyCodes)}";
+    }
+
+    #endregion
+}
+
+public static class KeyboardExtensions
+{
+    extension(KeyboardShortcut shortcut)
+    {
+        public bool IsKeyDown()
         {
             return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
         }
 
-        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        public bool IsKeyHeld()
         {
             return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
         }
     }
+}
 
-    public static class ToggleExtentions
+public static class ToggleExtentions
+{
+    extension(ServerSyncModTemplatePlugin.Toggle value)
     {
-        public static bool IsOn(this ServerSyncModTemplatePlugin.Toggle value)
+        public bool IsOn()
         {
             return value == ServerSyncModTemplatePlugin.Toggle.On;
         }
 
-        public static bool IsOff(this ServerSyncModTemplatePlugin.Toggle value)
+        public bool IsOff()
         {
             return value == ServerSyncModTemplatePlugin.Toggle.Off;
         }
--- VersionHandshake.cs
+++ VersionHandshake.cs
@@ -1,104 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
 using HarmonyLib;
 
-namespace ServerSyncModTemplate
+namespace ServerSyncModTemplate;
+
+[HarmonyPatch(typeof(ZNet), nameof(ZNet.OnNewConnection))]
+public static class RegisterAndCheckVersion
 {
-    [HarmonyPatch(typeof(ZNet), nameof(ZNet.OnNewConnection))]
-    public static class RegisterAndCheckVersion
+    private static void Prefix(ZNetPeer peer, ref ZNet __instance)
     {
-        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
-        {
-            // Register version check call
-            ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogDebug("Registering version RPC handler");
-            peer.m_rpc.Register($"{ServerSyncModTemplatePlugin.ModName}_VersionCheck",
-                new Action<ZRpc, ZPackage>(RpcHandlers.RPC_ServerSyncModTemplate_Version));
+        // Register version check call
+        ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogDebug("Registering version RPC handler");
+        peer.m_rpc.Register($"{ServerSyncModTemplatePlugin.ModName}_VersionCheck", new Action<ZRpc, ZPackage>(RpcHandlers.RPC_ServerSyncModTemplate_Version));
 
-            // Make calls to check versions
-            ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo("Invoking version check");
-            ZPackage zpackage = new();
-            zpackage.Write(ServerSyncModTemplatePlugin.ModVersion);
-            peer.m_rpc.Invoke($"{ServerSyncModTemplatePlugin.ModName}_VersionCheck", zpackage);
-        }
+        // Make calls to check versions
+        ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo("Invoking version check");
+        ZPackage zpackage = new();
+        zpackage.Write(ServerSyncModTemplatePlugin.ModVersion);
+        peer.m_rpc.Invoke($"{ServerSyncModTemplatePlugin.ModName}_VersionCheck", zpackage);
+    }
+}
+
+[HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_PeerInfo))]
+public static class VerifyClient
+{
+    private static bool Prefix(ZRpc rpc, ZPackage pkg, ref ZNet __instance)
+    {
+        if (!__instance.IsServer() || RpcHandlers.ValidatedPeers.Contains(rpc)) return true;
+        // Disconnect peer if they didn't send mod version at all
+        ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogWarning($"Peer ({rpc.m_socket.GetHostName()}) never sent version or couldn't due to previous disconnect, disconnecting");
+        rpc.Invoke("Error", 3);
+        return false; // Prevent calling underlying method
     }
 
-    [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_PeerInfo))]
-    public static class VerifyClient
+    private static void Postfix(ZNet __instance)
     {
-        private static bool Prefix(ZRpc rpc, ZPackage pkg, ref ZNet __instance)
+        ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.instance.GetServerPeerID(), $"{ServerSyncModTemplatePlugin.ModName}RequestAdminSync", new ZPackage());
+    }
+}
+
+[HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.ShowConnectError))]
+public class ShowConnectionError
+{
+    private static void Postfix(FejdStartup __instance)
+    {
+        if (__instance.m_connectionFailedPanel.activeSelf)
         {
-            if (!__instance.IsServer() || RpcHandlers.ValidatedPeers.Contains(rpc)) return true;
-            // Disconnect peer if they didn't send mod version at all
-            ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogWarning($"Peer ({rpc.m_socket.GetHostName()}) never sent version or couldn't due to previous disconnect, disconnecting");
+            __instance.m_connectionFailedError.fontSizeMax = 25;
+            __instance.m_connectionFailedError.fontSizeMin = 15;
+            __instance.m_connectionFailedError.text += $"\n{ServerSyncModTemplatePlugin.ConnectionError}";
+        }
+    }
+}
+
+[HarmonyPatch(typeof(ZNet), nameof(ZNet.Disconnect))]
+public static class RemoveDisconnectedPeerFromVerified
+{
+    private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+    {
+        if (!__instance.IsServer()) return;
+        // Remove peer from validated list
+        ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo($"Peer ({peer.m_rpc.m_socket.GetHostName()}) disconnected, removing from validated list");
+        _ = RpcHandlers.ValidatedPeers.Remove(peer.m_rpc);
+    }
+}
+
+public static class RpcHandlers
+{
+    public static readonly List<ZRpc> ValidatedPeers = new();
+
+    public static void RPC_ServerSyncModTemplate_Version(ZRpc rpc, ZPackage pkg)
+    {
+        string? version = pkg.ReadString();
+        ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo($"Version check, local: {ServerSyncModTemplatePlugin.ModVersion},  remote: {version}");
+        if (version != ServerSyncModTemplatePlugin.ModVersion)
+        {
+            ServerSyncModTemplatePlugin.ConnectionError = $"{ServerSyncModTemplatePlugin.ModName} Installed: {ServerSyncModTemplatePlugin.ModVersion}\n Needed: {version}";
+            if (!ZNet.instance.IsServer()) return;
+            // Different versions - force disconnect client from server
+            ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogWarning($"Peer ({rpc.m_socket.GetHostName()}) has incompatible version, disconnecting...");
             rpc.Invoke("Error", 3);
-            return false; // Prevent calling underlying method
         }
-
-        private static void Postfix(ZNet __instance)
+        else
         {
-            ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.instance.GetServerPeerID(), $"{ServerSyncModTemplatePlugin.ModName}RequestAdminSync", new ZPackage());
-        }
-    }
-
-    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.ShowConnectError))]
-    public class ShowConnectionError
-    {
-        private static void Postfix(FejdStartup __instance)
-        {
-            if (__instance.m_connectionFailedPanel.activeSelf)
+            if (!ZNet.instance.IsServer())
             {
-                __instance.m_connectionFailedError.fontSizeMax = 25;
-                __instance.m_connectionFailedError.fontSizeMin = 15;
-                __instance.m_connectionFailedError.text += $"\n{ServerSyncModTemplatePlugin.ConnectionError}";
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(ZNet), nameof(ZNet.Disconnect))]
-    public static class RemoveDisconnectedPeerFromVerified
-    {
-        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
-        {
-            if (!__instance.IsServer()) return;
-            // Remove peer from validated list
-            ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo($"Peer ({peer.m_rpc.m_socket.GetHostName()}) disconnected, removing from validated list");
-            _ = RpcHandlers.ValidatedPeers.Remove(peer.m_rpc);
-        }
-    }
-
-    public static class RpcHandlers
-    {
-        public static readonly List<ZRpc> ValidatedPeers = new();
-
-        public static void RPC_ServerSyncModTemplate_Version(ZRpc rpc, ZPackage pkg)
-        {
-            string? version = pkg.ReadString();
-            ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo($"Version check, local: {ServerSyncModTemplatePlugin.ModVersion},  remote: {version}");
-            if (version != ServerSyncModTemplatePlugin.ModVersion)
-            {
-                ServerSyncModTemplatePlugin.ConnectionError = $"{ServerSyncModTemplatePlugin.ModName} Installed: {ServerSyncModTemplatePlugin.ModVersion}\n Needed: {version}";
-                if (!ZNet.instance.IsServer()) return;
-                // Different versions - force disconnect client from server
-                ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogWarning($"Peer ({rpc.m_socket.GetHostName()}) has incompatible version, disconnecting...");
-                rpc.Invoke("Error", 3);
+                // Enable mod on client if versions match
+                ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo("Received same version from server!");
             }
             else
             {
-                if (!ZNet.instance.IsServer())
-                {
-                    // Enable mod on client if versions match
-                    ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo("Received same version from server!");
-                }
-                else
-                {
-                    // Add client to validated list
-                    ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo($"Adding peer ({rpc.m_socket.GetHostName()}) to validated list");
-                    ValidatedPeers.Add(rpc);
-                }
+                // Add client to validated list
+                ServerSyncModTemplatePlugin.ServerSyncModTemplateLogger.LogInfo($"Adding peer ({rpc.m_socket.GetHostName()}) to validated list");
+                ValidatedPeers.Add(rpc);
             }
         }
     }
